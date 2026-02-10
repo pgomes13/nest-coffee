ffae545934000dd31e75a0dcb17bd4b8e5d4283a
P Gomes <pritgomes@gmail.com>
2026-02-10 16:51:50 +1000

    feat: added mapped types

--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,6 +1,15 @@
+import { IsString, IsNumber, IsArray } from 'class-validator';
+
 export class CreateCoffeeDto {
+  @IsNumber()
   readonly id: number;
+
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+  
+  @IsArray({ each: true })
   readonly flavors: string[];
 }
