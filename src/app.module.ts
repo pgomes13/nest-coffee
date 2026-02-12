2510855f477da24abbf2c53858bc587f81b0f2b4
P Gomes <pritgomes@gmail.com>
2026-02-12 16:20:41 +1000

    feat: added env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,22 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'postgres',
-    autoLoadEntities: true,
-    synchronize: true,
-  }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
