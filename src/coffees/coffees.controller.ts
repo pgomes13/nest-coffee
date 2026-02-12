72ded1e91d9181f178d1b5f7e88b2b64b9b0959a
P Gomes <pritgomes@gmail.com>
2026-02-12 15:49:15 +1000

    fix: added dynamic modules

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Param, Post, Body, HttpStatus, HttpCode, Res, Patch, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  HttpStatus,
+  HttpCode,
+  Res,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -6,30 +18,30 @@ import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
-	constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(private readonly coffeesService: CoffeesService) {}
 
-	@Get()
+  @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
-		return this.coffeesService.findAll(paginationQuery);
-	}
+    return this.coffeesService.findAll(paginationQuery);
+  }
 
-	@Get(':id')
-	findOne(@Param('id') id: number) {
-		return this.coffeesService.findOne(id);
-	}
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.coffeesService.findOne(id);
+  }
 
-	@Post()
-	create(@Body() createCoffeeDto: CreateCoffeeDto) {
-		return this.coffeesService.create(createCoffeeDto);
-	}
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
+  }
 
-	@Patch(':id')
-	update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
-		return this.coffeesService.update(id, updateCoffeeDto);
-	}
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
 
-	@Delete(':id')
-	remove(@Param('id') id: number) {
-		return this.coffeesService.remove(id);
-	}
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.coffeesService.remove(id);
+  }
 }
