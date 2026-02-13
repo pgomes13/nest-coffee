a8bf877047158f1640ca02b1ea4d0519cf51b2ee
P Gomes <pritgomes@gmail.com>
2026-02-13 15:57:07 +1000

    stable commit

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -3,12 +3,13 @@ import {
 	NotAcceptableException,
 	NotFoundException,
 } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
 import { Coffee } from './entities/coffee.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
-import { InjectConnection, InjectModel } from '@nestjs/mongoose';
-import { Connection, Model } from 'mongoose';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { Event } from '../events/entities/event.entity';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 
 @Injectable()
 export class CoffeesService {
