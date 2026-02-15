69295635437d873f22f7a1ca9588dc4120a5688a
P Gomes <pritgomes@gmail.com>
2026-02-15 14:58:33 +1000

    feat: added iam module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,35 +7,33 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
-import { AuthenticationController } from './iam/authentication/authentication.controller';
-import { AuthenticationService } from './iam/authentication/authentication.service';
 import { IamModule } from './iam/iam.module';
 import appConfig from './config/app.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [appConfig],
-    }), // Load environment variables from .env file
-    CoffeesModule,
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT),
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
-    DatabaseModule,
-    CommonModule,
-    UsersModule,
-    IamModule,
-  ],
-  controllers: [AppController, AuthenticationController],
-  providers: [AppService, AuthenticationService],
+	imports: [
+		ConfigModule.forRoot({
+			load: [appConfig],
+		}), // Load environment variables from .env file
+		CoffeesModule,
+		TypeOrmModule.forRootAsync({
+			useFactory: () => ({
+				type: 'postgres',
+				host: process.env.DATABASE_HOST,
+				port: Number(process.env.DATABASE_PORT),
+				username: process.env.DATABASE_USER,
+				password: process.env.DATABASE_PASSWORD,
+				database: process.env.DATABASE_NAME,
+				autoLoadEntities: true,
+				synchronize: true,
+			}),
+		}),
+		DatabaseModule,
+		CommonModule,
+		UsersModule,
+		IamModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { AuthenticationService } from './authentication/authentication.service';
+import { AuthenticationController } from './authentication/authentication.controller';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([User])],
+	controllers: [AuthenticationController],
 	providers: [
+		AuthenticationService,
 		{
 			provide: HashingService,
 			useClass: BcryptService,
 		},
 	],
+	exports: [AuthenticationService],
 })
 export class IamModule {}
